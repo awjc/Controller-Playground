90eb34fcf5457b68987a57a5891b64abdc058733
awjc <2312902+awjc@users.noreply.github.com>
2023-07-05 23:40:24 -0700

    Saving & Loading actions

--- Assets/Scripts/System/SaverLoader.cs
+++ Assets/Scripts/System/SaverLoader.cs
@@ -6,20 +6,32 @@ using UnityEngine.InputSystem;
 public class SaverLoader : MonoBehaviour
 {
 
-  public InputActionAsset primaryActions;
+  public InputActionAsset actions;
 
   private InputActionMap gameplayActionMap;
 
+  private List<InputAction> allActions;
+
   void Start()
   {
-    gameplayActionMap = primaryActions.FindActionMap("SaveLoad", true);
-    if (gameplayActionMap == null) { return; }
+    gameplayActionMap = actions.FindActionMap("SaveLoad", true);
+    if (gameplayActionMap == null)
+    {
+      Debug.Log("Couldn't find SaveLoad action map. Aborting.");
+      return;
+    }
 
     var saveAction = gameplayActionMap.FindAction("Save");
     saveAction.performed += context => DoSave();
 
     var loadAction = gameplayActionMap.FindAction("Load");
     loadAction.performed += context => DoLoad();
+
+    allActions = new List<InputAction> {
+      saveAction, loadAction
+    };
+
+    Debug.Log("SaverLoader controls registered successfully.");
   }
 
   public void DoSave()
@@ -30,5 +42,29 @@ public class SaverLoader : MonoBehaviour
   public void DoLoad()
   {
     Debug.Log("Loading");
+  }
+
+  private void OnEnable()
+  {
+    actions?.Enable();
+    if (allActions != null)
+    {
+      foreach (var action in allActions)
+      {
+        action?.Enable();
+      }
+    }
+  }
+
+  private void OnDisable()
+  {
+    actions?.Disable();
+    if (allActions != null)
+    {
+      foreach (var action in allActions)
+      {
+        action?.Disable();
+      }
+    }
   }
 }
